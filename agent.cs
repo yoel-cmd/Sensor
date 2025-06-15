ad7b47cd45499a7056ad870599e9a7814b54b227
yoel <yoeli5746@gmail.com>
2025-06-15 14:35:09 +0300

    Add project files.

---
+++ agent.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sensor
+{
+    abstract class agent
+    {
+
+        //public string type { get;set; }
+        public string rank{ get; set; }
+        public Dictionary<string, int> arrRank = new Dictionary<string, int>
+        {
+            { "junior", 1 },
+            { "senior", 2 },
+            { "commander", 3 }
+        };
+        public sensor[] sensors ;
+
+        public agent(string rank)
+        {
+            this.rank = rank;
+            if (!arrRank.ContainsKey(rank))
+            {
+                throw new ArgumentException("Rank not found");
+            }
+            else
+            {
+                sensors = new sensor[arrRank[rank]];
+            }
+        }
+    }
+}
